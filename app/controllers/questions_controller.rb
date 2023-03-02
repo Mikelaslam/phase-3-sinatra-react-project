b58586206510d97e7340f5442283967485afb676
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-02 13:13:34 +0300

    Post questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,28 +1,33 @@
 class QuestionsController < ApplicationController
     #routings
     get "/questions" do
-      @questions = Question.all
-      posts.to_json
+      questions = Question.all
+      questions.to_json
     end
-    post "questions" do
-    tittle = params[:tittle]
-    category = params[:category]
-    description = params[:category]
-    user = params [:user]
 
-    if(tittle.present? && category.present? && description.present? && user.present?)
-        post = Post.create(tittle: tittle, category: category, description: description, user: user)
-        if post
-            message = {:succcess => "Post created successfully!"}
-                        message.to_json
-                    else
-                        message = {:error => "Error saving user!"}
-                        message.to_json
-                    end
-                else
-                endmessage = {:error => "All fields should be filled!"}
-                    message.to_json
-                end
-            end
+    post "/questions" do
+
+    title = params[:title]
+    category = params[:category]
+    description = params[:description]
+    user_id = params[:user]
+
+    if title.present? && category.present? && description.present? && user_id.present?
+        question = Question.create(title: title, category: category, description: description, user_id: user_id)
+        if question
+            message = { success: "Question created successfully!" }
+        else
+            message = { error: "Error saving Question!" }
+        end
+        else
+        message = { error: "All fields should be filled!" }
+    end
+
+    message.to_json
+end
+
+#edit question
+     
     
 end
+
