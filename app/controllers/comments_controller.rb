e3c7b4a9299e099db1139f4dc571391c92c90af5
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-02 13:54:05 +0300

    Post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,5 +4,24 @@
      comments = Comments.all
      comments.to_json
 end
+post "/comments" do
+        comment = params[:comment]
+        post = params[:question]
+        user = params[:user]
+
+        if(comment.present? && post.present? && user.present?)
+            comment = Comment.create(comment: comment, question_id: post, user_id: user)
+            if comment
+                message = {:succcess => "Comment created successfully!!"}
+                message.to_json
+            else
+                message = {:error => "Error saving comment!"}
+                message.to_json
+            end
+        else
+            message = {:error => "All fields should be field!"}
+            message.to_json
+        end
+    end
 
 end
