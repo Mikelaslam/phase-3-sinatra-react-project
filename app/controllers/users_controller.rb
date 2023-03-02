42b93694416a3e64ac8335293edbf94abe279a15
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-02 11:21:25 +0300

    Updating post users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,43 @@
 class UsersController < ApplicationController
 
     # Routings
+   get "/users" do
+    users = User.all
+    users.to_json
+   end
 
-        get "/users" do
-           users = User.all
-           users.to_json
-        end
+   post "/users" do
 
-        post "/users" do
-            name_ = params[:name]
-            user_name = params[:username]
-            email_ = params[:email]
-            phone_ = params[:phone]
-            
+  name = params[:name]
+  username = params[:username]
+  phone = params[:phone]
+  email = params[:email]
 
-            if(name_.present? && user_name.present? && phone_.present? && email_.present?)
+  if name.present? && username.present? && phone.present? && email.present?
+    check_email_exists = User.where(email: email).count()
 
-                check_email_exists = User.where(email: email_).count() 
-                 puts("XXXXXXXXXXXXXXXXX ", check_email_exists)
+    if check_email_exists < 1
+      user = User.create(name: name, username: username, email: email, phone: phone)
+      
+      if user
+        message = { success: "User created successfully!!" }
+        return message.to_json
+      else
+        message = { error: "Error saving user!" }
+        return message.to_json
+      end
+     else
+      message = { error: "Email exists!" }
+      return message.to_json
+    end
+  else
+    message = { error: "All fields should be filled!" }
+    return message.to_json
+  end
+end
 
-                 if check_email_exists < 1
-                    user = User.create(name: name_, username: user_name, email: email_, phone: phone_)
-                    if user
-                        message = {:succcess => "User created successfully!!"}
-                        message.to_json
-                    else
-                        message = {:error => "Error saving user!"}
-                        message.to_json
-                    end
-                else
-                    message = {:error => "Email exists!"}
-                    message.to_json
-                end
-            else
-                message = {:error => "All fields should be field!"}
-                message.to_json
-            end
 
-        end
+
 
         #delete user
          delete "/users/:id" do
