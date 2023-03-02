1264227818abfe6e32755ed0ed73c1ca48868f16
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-02 13:36:35 +0300

    Updating users

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,7 +27,28 @@ class QuestionsController < ApplicationController
 end
 
 #edit question
-     
+     patch '/questions/:id' do
+  check_question_existence = Question.exists?(id: params[:id]) #true/false
+  if check_question_existence
+    title = params[:title]
+    content = params[:content]
+    user = params[:user]
+    if (title.present? && category.present? && description.present? && user.present?)
+      # updating
+      question = Question.find_by(id: params[:id])
+      question.update(title: title, category: category, description: description, user_id: user)
+      message = { success: "Question updated successfully!" }
+      return message.to_json
+    else
+      message = { error: "All fields should be filled!" }
+      return message.to_json
+    end
+  else
+    message = { error: "The question does not exist!" }
+    return message.to_json
+  end
+end
+
     
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,55 @@
 class UsersController < ApplicationController
 
-    # Routings
-   get "/users" do
+  # Get all users
+  get "/users" do
     users = User.all
     users.to_json
-   end
-
-   post "/users" do
-
-  name = params[:name]
-  username = params[:username]
-  phone = params[:phone]
-  email = params[:email]
-
-  if name.present? && username.present? && phone.present? && email.present?
-    check_email_exists = User.where(email: email).count()
-
-    if check_email_exists < 1
-      user = User.create(name: name, username: username, email: email, phone: phone)
-      
-      if user
-        message = { success: "User created successfully!!" }
-        return message.to_json
-      else
-        message = { error: "Error saving user!" }
-        return message.to_json
-      end
-     else
-      message = { error: "Email exists!" }
-      return message.to_json
-    end
-  else
-    message = { error: "All fields should be filled!" }
-    return message.to_json
   end
-end
 
+  # Create new user
+  post "/users" do
+    name = params[:name]
+    username = params[:username]
+    phone = params[:phone]
+    email = params[:email]
 
+    if name.present? && username.present? && phone.present?
+      check_quest
+      check_email_exists = User.where(email: email).count()
 
+      if check_email_exists < 1
+        user = User.create(name: name, username: username, email: email, phone: phone)
 
-        #delete user
-         delete "/users/:id" do
-        count_users = User.where(id: params[:id]).count() 
-        if count_users>0
-            user = User.find(params[:id])
-            user.destroy
-            message = {:succcess => "User deleted successfully!!"}
-            message.to_json
-
+        if user
+          message = { success: "User created successfully!!" }
+          message.to_json
         else
-            message = {:error => "User does not exist!"}
-            message.to_json
+          message = { error: "Error saving user!" }
+          message.to_json
         end
-
-
+      else
+        message = { error: "Email exists!" }
+        message.to_json
+      end
+    else
+      message = { error: "All fields should be filled!" }
+      message.to_json
     end
+  end
+
+  # Delete user
+  delete "/users/:id" do
+    count_users = User.where(id: params[:id]).count() 
+    if count_users > 0
+      user = User.find(params[:id])
+      user.destroy
+      message = { success: "User deleted successfully!!" }
+      message.to_json
+    else
+      message = { error: "User does not exist!" }
+      message.to_json
+    end
+  end
+
 end
+
